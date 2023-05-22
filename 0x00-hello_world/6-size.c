bd3c3feb70df63f28e3825a8893215ae29540e0b
asresgamu <asresgamu@gmail.com>
2023-05-22 12:30:50 -0700

    Printing size of data in C programming using sixeof() method

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ *
+ *
+ * main -Printing size of data in cprogramming
+ *
+ * Return o;
+ */
+int main(){
+	printf("Size of char:%ld byte(s)\n", sizeof(char));
+	printf("Size of int :%id bytes(s) \n", sizeof(int));
+	printf("Size of float:%id byte(s)\n", sizeof(float));
+	printf("Size of double:%id byte(s)", sizeof(double));
+
+	return 0;
+
+
+
+}
+
