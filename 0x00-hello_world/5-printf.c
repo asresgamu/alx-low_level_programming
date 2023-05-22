51c3806d55423757ed8de258283d9d5e2990d77b
asresgamu <asresgamu@gmail.com>
2023-05-22 11:06:33 -0700

    Printing c prongraming using printf with new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h> 
+/**
+ * main -This programming print with proper grammar, but the outcome is a piece of art,\n
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
