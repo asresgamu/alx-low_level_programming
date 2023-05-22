08541c1b14316b8b09eb13e5db37bff3a180f3bf
asresgamu <asresgamu@gmail.com>
2023-05-22 12:54:40 -0700

    sizeof() methods used to print dtat size in c programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
 /**
- *
- *
  * main -Printing size of data in cprogramming
  *
  * Return o;
  */
 int main(viod)
 {
-	printf("Size of char:%1 byte(s)\n", sizeof(char));
-	printf("Size of int :%4d bytes(s) \n", sizeof(int));
-	printf("Size of float:%4d byte(s)\n", sizeof(float));
-	printf("Size of double:%8d byte(s)", sizeof(double));
+	printf("Size of char:%c byte(s)\n", sizeof(char));
+	printf("Size of int :%d bytes(s) \n", sizeof(int));
+	printf("Size of float:%f byte(s)\n", sizeof(float));
+	printf("Size of double:%lf  byte(s)\n", sizeof(double));
 
 	return ();
 
