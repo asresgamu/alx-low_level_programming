4c7a517029ebd3bba0df9809125824cb007a29a9
asresgamu <asresgamu@gmail.com>
2023-05-24 01:53:07 -0700

    My 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
   * main - This is the starting point of programing.
   *
@@ -9,7 +8,7 @@ int main(void)
 {
 	_putchar('_');
 	_putchar('p');
- 	_putchar('u');
+	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
