6c0cab47a76784b5d07fb46693091b89c4b18f7b
asresgamu <asresgamu@gmail.com>
2023-06-03 23:08:21 -0700

    1001

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point of programming
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return: 0
  */
 int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
 }
