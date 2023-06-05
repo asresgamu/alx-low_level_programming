249664f799d42e4dd5c7c1eea5a7fd7f94b72441
asresgamu <asresgamu@gmail.com>
2023-06-04 20:13:00 -0700

    Printing positive or negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @ab: the int to check
+ * Return: 1 and prints + if ab is greater than zero
+ * 0 and prints 0 if ab is zero
+ * -1 and prints - if ab is less than zero
+ */
+	int print_sign(int ab)
+	{
+		if (ab > 0)
+		{
+			_putchar('+');
+			return (1);
+		} else if (ab == 0)
+		{
+			_putchar(48);
+			return (0);
+		} else if (ab < 0)
+		{
+			_putchar('-');
+		}
+			return (-1);
+	}
+
+
